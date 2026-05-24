d44f27dc7529b2d121e046b92fe12d35f714888b
Chengmun58 <moon@mumsme.com>
2026-05-24 16:49:57 +0800

    Simplify app routing for public AI launch

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,37 +1,15 @@
-import { Toaster } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import NotFound from "@/pages/NotFound";
 import { Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
-import { ThemeProvider } from "./contexts/ThemeContext";
-import Home from "./pages/Home";
-import FilesPage from "./pages/FilesPage";
-import DailyContentPage from "./pages/DailyContentPage";
-import ImmersionUpdates from "./pages/ImmersionUpdates";
 import KoreanGenzPage from "./pages/KoreanGenzPage";
 import AIKoreanEnginePage from "./pages/AIKoreanEnginePage";
-import DashboardLayout from "./components/DashboardLayout";
 
 function Router() {
   return (
     <Switch>
-      <Route path={"/"} component={Home} />
-      <Route path={"/korean-genz"} component={KoreanGenzPage} />
+      <Route path={"/"} component={AIKoreanEnginePage} />
       <Route path={"/ai-korean"} component={AIKoreanEnginePage} />
-      <Route path={"/immersion-updates"} component={ImmersionUpdates} />
-      <Route path={"/files"}>
-        <DashboardLayout>
-          <FilesPage />
-        </DashboardLayout>
-      </Route>
-      <Route path={"/daily"}>
-        <DashboardLayout>
-          <DailyContentPage />
-        </DashboardLayout>
-      </Route>
-      <Route path={"/404"} component={NotFound} />
-      {/* Final fallback route */}
-      <Route component={NotFound} />
+      <Route path={"/korean-genz"} component={KoreanGenzPage} />
+      <Route component={AIKoreanEnginePage} />
     </Switch>
   );
 }
@@ -39,14 +17,7 @@ function Router() {
 function App() {
   return (
     <ErrorBoundary>
-      <ThemeProvider
-        defaultTheme="light"
-      >
-        <TooltipProvider>
-          <Toaster />
-          <Router />
-        </TooltipProvider>
-      </ThemeProvider>
+      <Router />
     </ErrorBoundary>
   );
 }
